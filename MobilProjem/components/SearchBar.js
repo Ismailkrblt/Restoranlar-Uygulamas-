ce08da182efdfb52e716b0bdab195fdd153a4845
Ismailkrblt <162838395+Ismailkrblt@users.noreply.github.com>
2024-03-22 11:38:02 +0300

    upload

--- MobilProjem/components/SearchBar.js
+++ MobilProjem/components/SearchBar.js
@@ -1,17 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
-
-
-
-
 export default function App() {
-    return (
-     <View>
-         <Text><AntDesign name="search1" size={24} color="black" /></Text>
-     </View>
-    );
-  }
-  
-  const styles = StyleSheet.create({})
+  return (
+    <View style={styles.backgraoundStyle}>
+      <AntDesign style={styles.iconStyle} name="search1" size={30} color="black" />
+     <TextInput style={styles.inputStyle} placeholder="Ara" autoCorrect={false} autoCapitalize='none' />
+   </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  backgraoundStyle: {
+    backgroundColor: 'lightgray',
+    flexDirection: 'row',
+    margin: 10,
+    height:50,
+    alignItems: 'center'
+    flex: 1, 
+    justifyContent: 
+    alignItems,
+    borderRadius:20,
+  },
+  iconStyle:{
+    marginHorizontal:15,
+
+  },
+  inputStyle : {
+    flex:1,
+    fontSize:18,
+  },
+});
