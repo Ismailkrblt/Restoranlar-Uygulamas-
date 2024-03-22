10526c577450a92411f2b9c0bd61978b0a16414b
Ismailkrblt <162838395+Ismailkrblt@users.noreply.github.com>
2024-03-22 10:42:31 +0300

    upload

--- MobilProjem/components/SearchBar.js
+++ MobilProjem/components/SearchBar.js
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { AntDesign } from '@expo/vector-icons';
+
+
+
+
 
 export default function App() {
     return (
      <View>
-         <Text>SearchBar</Text>
+         <Text><AntDesign name="search1" size={24} color="black" /></Text>
      </View>
     );
   }
