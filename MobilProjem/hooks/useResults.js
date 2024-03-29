d2eedfae94fcb376165a5f6c1f1a27a01e9751b6
Ismailkrblt <162838395+Ismailkrblt@users.noreply.github.com>
2024-03-29 11:59:22 +0300

    upload

--- MobilProjem/hooks/useResults.js
+++ MobilProjem/hooks/useResults.js
@@ -1,19 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import yelp from '../api/yelp';
 
 export default () => {
+  const [results, setResults] = useState([]);
   const searchApi = async (searchTerm) => {
-    await yelp.get('/search', {
+    const response = await yelp.get('/search', {
       params: {
         limit: 50,
         term: searchTerm,
         location: 'İstanbul',
       },
     });
+    setResults(response.data.businesses);
   };
+
   useEffect(() => {
     searchApi('Toast');
   }, []);
 
-  return [searchApi];
+  return [searchApi, results];
 };
--- MobilProjem/screens/SearchScreen.js
+++ MobilProjem/screens/SearchScreen.js
@@ -1,11 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
+
+
 
 export default function SearchScreen() {
+  const [searchApi,results] = useResults()
+  console.log(results)
+  
+  const filterResultsByPrice = (price) => {
+    return results.filter((results)=>{
+      return results.price === price;
+  });
+};
   return (
     <View>
       <SearchBar />
+      <ResultsList title="Uygun Fiyatlı Restaurantlar" results={filterResultsByPrice('₺')}/>
+      <ResultsList title="Ortalama Fiyatlı Restaurantlar" results={filterResultsByPrice('₺₺')}/>
+      <ResultsList title="Pahalı Fiyatlı Restaurantlar" results={filterResultsByPrice('₺₺₺')}/>
     </View>
   );
 }
