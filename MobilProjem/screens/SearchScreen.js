8f9f76f3fd6dc6fdda6203b23620e5774e462fb0
Ismailkrblt <162838395+Ismailkrblt@users.noreply.github.com>
2024-03-29 12:02:36 +0300

    upload

--- MobilProjem/screens/SearchScreen.js
+++ MobilProjem/screens/SearchScreen.js
@@ -2,24 +2,32 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
-
-
+import ResultsList from '../components/ResultsList';
 
 export default function SearchScreen() {
-  const [searchApi,results] = useResults()
-  console.log(results)
-  
+  const [searchApi, results] = useResults();
+  console.log(results);
+
   const filterResultsByPrice = (price) => {
-    return results.filter((results)=>{
-      return results.price === price;
-  });
-};
+    return results.filter((result) => {
+      return result.price === price;
+    });
+  };
   return (
     <View>
       <SearchBar />
-      <ResultsList title="Uygun Fiyatlı Restaurantlar" results={filterResultsByPrice('₺')}/>
-      <ResultsList title="Ortalama Fiyatlı Restaurantlar" results={filterResultsByPrice('₺₺')}/>
-      <ResultsList title="Pahalı Fiyatlı Restaurantlar" results={filterResultsByPrice('₺₺₺')}/>
+      <ResultsList
+        title="Ucuz Restoranlar"
+        results={filterResultsByPrice('₺')}
+      />
+      <ResultsList
+        title="Uygun Restoranlar"
+        results={filterResultsByPrice('₺₺')}
+      />
+      <ResultsList
+        title="Pahalı Restoranlar"
+        results={filterResultsByPrice('₺₺₺')}
+      />
     </View>
   );
 }
